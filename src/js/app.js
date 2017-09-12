f3d10601e1765a94c5d9b5836cd19d29e02cc7d5
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-09-12 22:01:40 +0100

    Wrapping major functionality in functions

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import Deck from './equipment/cards/deck';
 import Dealer from './people/dealer';
 import Player from './people/player';
 
+let gameInProgress = false;
+
 // create a deck of cards utilising Deck class (only 1 at the moment)
 var deck = new Deck();
 window.deck = deck; // temp
@@ -17,60 +19,81 @@ window.participants = participants; // temp
 const dealer = new Dealer(0);
 participants.push(dealer);
 
-const dealCard = (person) => {
-	deck.cards = dealer.dealCard(person, deck.cards);
-};
-
 // create a player with methods (class)
 const player = new Player(0);
 participants.push(player);
 
-// pick cards, alternating between dealer and player until they have 2
-for (let i=0; i<=1; i++) {
-	for (let participant of participants) {
-		dealCard(participant);
-		participant.playing = true;
+// TODO trigger with 'deal' button
+gameInProgress = true;
 
-		// calculate total values and present user option to hit/stick
-		participant.cardTotal = 0;
-		for (let participantCard of participant.cards) {
-			if (participantCard.values.length > 1) {
-				participant.cardTotal += 11; // for now, onload will assume 11 to achieve blackjack on first deal
-			} else {
-				participant.cardTotal += participantCard.values[0];
+const dealRoundOfCards = (initial) => {
+
+	// deal card function to utilise the dealer to distribute cards
+	const dealCard = (person) => {
+		deck.cards = dealer.dealCard(person, deck.cards);
+	};
+
+	// deal cards, alternating between dealer and player until they have 2 each
+	for (let i=0; i <= (initial === true ? 2 : 1); i++) {
+		for (let participant of participants) {
+			dealCard(participant);
+			participant.playing = true;
+
+			// calculate total values and present user option to hit/stick
+			participant.cardTotal = 0;
+			for (let participantCard of participant.cards) {
+				if (participantCard.values.length > 1) {
+					participant.cardTotal += 11; // for now, onload will assume 11 to achieve blackjack on first deal
+				} else {
+					participant.cardTotal += participantCard.values[0];
+				}
 			}
 		}
 	}
 }
+dealRoundOfCards(true);
 
 // check game status, after cards have been dealt
 let winners = [];
-for (let participant of participants) {
-	switch (true) {
 
-		case (!participant.playing):
-			continue;
-			break;
+let checkGameStatus = () => {
+	for (let participant of participants) {
+		switch (true) {
 
-		case (participant.cardTotal < 21):
-			continue; // TODO check hit/stick state
-			break;
+			case (!participant.playing):
+				continue;
+				break;
 
-		case (participant.cardTotal === 21):
-			winners.push(participant);
-			break;
+			case (participant.cardTotal < 21):
+				continue; // TODO check hit/stick state
+				break;
 
-		case (participant.cardTotal > 21):
-			participant.playing = false;
-			console.log(`${participant.id} is out of the game with a score of ${winner.cardTotal}.`);
-			break;
+			case (participant.cardTotal === 21):
+				winners.push(participant);
+				break;
 
+			case (participant.cardTotal > 21):
+				participant.playing = false;
+				console.log(`${participant.id} is out of the game with a score of ${winner.cardTotal}.`);
+				break;
+
+		}
 	}
+	if (winners.length) winners.forEach((winner) => {
+		console.log(`${winner.id} wins with a score of ${winner.cardTotal}!`);
+	});
 }
-if (winners.length) winners.forEach((winner) => {
-	console.log(`${winner.id} wins with a score of ${winner.cardTotal}!`);
-});
+
+checkGameStatus();
 
 // add event listeners for events above
+/*const buttons = document.querySelectorAll('.actions button');
+buttons.forEach((button) => {
+	button.addEventListener('click', (event) => {
+		// player wants to hit/stick
+		// event.target.getAttribute('data-hit');
+	});
+});*/
 
 // create a game class? holding situations to be in, controlling outcomes?
+// const game = new BlackjackGame();
